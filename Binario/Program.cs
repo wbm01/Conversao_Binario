14e2e62fa122c5f87ab4eb0b7533b1e39057217f
Willian Marioti <willian.marioti@gmail.com>
2023-03-06 15:20:41 -0300

    Add project files.

---
+++ Binario/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[] binario = new int[8];
+        int[] convertido = new int[8];
+        int valor, resto, quociente;
+
+        Console.WriteLine("Informe um número para versão em binário: ");
+        valor = int.Parse(Console.ReadLine());
+        convertido = converter(valor);
+
+
+        int[] converter(int valor)
+        {
+            quociente = valor;
+
+            for (int contador = 0; contador < binario.Length; contador++)
+            {
+                resto = quociente % 2;
+                binario[contador] = resto;
+                quociente = valor / 2;
+                valor = quociente;
+            }
+            return binario;
+        }
+
+        for(int contador = binario.Length-1 ;contador >= 0; contador--)
+        {
+            Console.Write(binario[contador] + " ");
+            
+        }
+
+    }
+}
